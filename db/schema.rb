7b76e2fd7cfa0b8a70855681cd9928862a8b0c6b
tchartchke <chartai.kl@gmail.com>
2021-03-15 11:03:47 -0500

    redoing users with devise

--- db/schema.rb
+++ db/schema.rb
@@ -17,11 +17,13 @@ ActiveRecord::Schema.define(version: 2021_03_15_154251) do
     t.string "uid"
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index "\"reset_password_token\"", name: "index_users_on_reset_password_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
