5ad91c8f88ce4a672e1d01b19df4726011247bed
tchartchke <chartai.kl@gmail.com>
2021-03-18 12:34:58 -0500

    cleaned up views folder and showing errors for login

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
 
 end
+
+#TODO: Delete if you get devise to work properly again
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,4 @@
 Rails.application.routes.draw do
-  # resources :answers
-  # resources :questions
-  # resources :rounds
   scope :quizzes do
     resources :rounds, only: [:create, :edit, :update, :destroy] do
       resources :questions, only: [:new, :create, :edit, :update, :destroy]
@@ -9,7 +6,12 @@ Rails.application.routes.draw do
   end
 
   resources :quizzes, only: [:show, :index, :new, :create, :edit, :update, :destroy]
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions', registrations: 'users/registrations'  }
+  
+  devise_for :users, controllers: { 
+    omniauth_callbacks: 'users/omniauth_callbacks', 
+    sessions: 'users/sessions', 
+    registrations: 'users/registrations'}
+  # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "application#welcome"
