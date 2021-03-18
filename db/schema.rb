1f6f1f6e11309c43f4639ce893f95da7c18cd391
tchartchke <chartai.kl@gmail.com>
2021-03-18 14:48:33 -0500

    game model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_15_205017) do
+ActiveRecord::Schema.define(version: 2021_03_18_174547) do
 
   create_table "answers", force: :cascade do |t|
     t.string "answer"
     t.integer "question_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "games", force: :cascade do |t|
+    t.integer "player_id"
+    t.integer "quiz_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
