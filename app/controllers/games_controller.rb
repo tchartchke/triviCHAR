155c74fc49fd8ea6687978cb2e4772092b6e455e
tchartchke <chartai.kl@gmail.com>
2021-03-18 21:17:33 -0500

    getting create button for game to route to correct place

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,2 +1,21 @@
 class GamesController < ApplicationController
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def create
+    raise params.inspect
+    game_params[:quiz_id] = params[:format]
+    @game = Game.new( game_params[:quiz_id] )
+  end
+
+  private
+
+  def game_params
+    params.require(:game).permit( :quiz_id, :player_id )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # resources :games
   scope :quizzes do
     resources :rounds, only: [:create, :edit, :update, :destroy] do
       resources :questions, only: [:new, :create, :edit, :update, :destroy]
@@ -7,13 +10,17 @@ Rails.application.routes.draw do
 
   resources :quizzes, only: [:show, :index, :new, :create, :edit, :update, :destroy]
   
+  get '/browse' => 'quizzes#play'
+
+  resources :games, only: [:index, :show, :create]
+
   devise_for :users, controllers: { 
     omniauth_callbacks: 'users/omniauth_callbacks', 
     sessions: 'users/sessions', 
     registrations: 'users/registrations'}
   # devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   root to: "application#welcome"
 
   get '/home' => "users#home", as: :user_root # creates user_root_path
