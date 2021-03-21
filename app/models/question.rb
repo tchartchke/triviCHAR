a86e5e02f8fec6b124d83a2ad13939c89e709423
tchartchke <chartai.kl@gmail.com>
2021-03-21 12:48:56 -0500

    fields_wth_errors validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :round
+  
   has_one :answer, dependent: :destroy
 
   accepts_nested_attributes_for :answer, reject_if: proc { |attributes| attributes['answer'].blank? }
