064bf4904c67848137ce5686c085c0bfad40ddfb
tchartchke <chartai.kl@gmail.com>
2021-03-25 16:49:49 -0500

    redirecting to home if already signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   def welcome
-    
+    if user_signed_in?
+      redirect_to user_root_path
+    else
+      render :welcome
+    end
   end
 
   protected
@@ -11,12 +15,4 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
 
-  # def after_sign_in_path_for(resource)
-    #     stored_location_for(resource) ||
-    #       if resource.is_a?(User) && resource.can_publish?
-    #         publisher_url
-    #       else
-    #         super
-    #       end
-      # end
 end
