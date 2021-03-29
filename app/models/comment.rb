1f2e8b90f4308ccfab20dd79c794981a32d2a5c9
tchartchke <chartai.kl@gmail.com>
2021-03-28 23:06:38 -0500

    adding comment validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,8 +3,22 @@ class Comment < ApplicationRecord
   belongs_to :quiz
 
   validates :comment, presence: true
+  validates :player, presence: true
+  validates :quiz, presence: true
+  validate :player_has_completed_game
 
   def host 
     quiz.host
+  end
+
+  private
+
+  def player_has_completed_game
+    g = player.games.find{ |game| game.quiz == quiz}
+    if g.nil?
+      errors.add(:player, 'has not played this game')
+    elsif g.status != 'complete'
+      errors.add(:player, 'can only comment on completed games')
+    end
   end
 end
