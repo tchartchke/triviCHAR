d72421fb228e724f35bfd971f0345a955769b6a4
tchartchke <chartai.kl@gmail.com>
2021-08-10 14:30:54 -0500

    still trying to fix tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,23 +1,31 @@
-const purgecss = [
-  "@fullhuman/postcss-purgecss",
-  {
-    content: ["./components/**/*.js", "./pages/**/*.js"],
-    defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-  }
-];
-module.exports = {
+// postcss.config.js
+let environment = {
   plugins: [
-    require('tailwindcss')("./app/javascript/stylesheets/tailwind.config.js"),
+    require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
     require('postcss-import'),
-    require('tailwindcss/colors'),
     require('postcss-flexbugs-fixes'),
-    require("autoprefixer"),
     require('postcss-preset-env')({
       autoprefixer: {
         flexbox: 'no-2009'
       },
       stage: 3
-    }),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    })
   ]
+};
+
+if (process.env.RAILS_ENV === 'production') {
+  environment.plugins.push(
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './app/**/.html.erb',
+        './app/helpers/**/*.rb',
+        './app/javascript/**/*.js',
+        './app/javascript/**/*.vue',
+        './app/javascript/**/*.jsx',
+      ],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
+  )
 }
+
+module.exports = environment;
