32ab305b5f9d5a89b71471900540b7653beb1355
tchartchke <chartai.kl@gmail.com>
2021-08-10 14:00:58 -0500

    compat tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -11,13 +11,13 @@ module.exports = {
     require('postcss-import'),
     require('tailwindcss/colors'),
     require('postcss-flexbugs-fixes'),
-    // require("autoprefixer"),
-    // require('postcss-preset-env')({
-    //   autoprefixer: {
-    //     flexbox: 'no-2009'
-    //   },
-    //   stage: 3
-    // }),
+    require("autoprefixer"),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    }),
     ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,5 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [
-    require("postcss-import"),
-    require("tailwindcss"),
-    require("autoprefixer")
-  ]
+  plugins: [],
 }
-
-
