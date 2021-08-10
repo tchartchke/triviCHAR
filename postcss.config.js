07e5ef1c79504d35629ebc04536bc1b63393449c
tchartchke <chartai.kl@gmail.com>
2021-08-10 14:53:49 -0500

    still trying to fix tailwind

--- postcss.config.js
+++ postcss.config.js
@@ -1,4 +1,8 @@
-// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')
+const cssnano = require('cssnano')
+
+
+
 let environment = {
   plugins: [
     require('tailwindcss')('./app/javascript/stylesheets/tailwind.config.js'),
@@ -9,23 +13,35 @@ let environment = {
         flexbox: 'no-2009'
       },
       stage: 3
+    }),
+    
+    
+    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
+    process.env.NODE_ENV === 'production'
+      ? cssnano({ preset: 'default' })
+      : null,
+    purgecss({
+      content: ['./layouts/**/*.html', './src/**/*.vue', './src/**/*.jsx'],
+      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
     })
+
+
   ]
 };
 
-if (process.env.RAILS_ENV === 'production') {
-  environment.plugins.push(
-    require('@fullhuman/postcss-purgecss')({
-      content: [
-        './app/**/.html.erb',
-        './app/helpers/**/*.rb',
-        './app/javascript/**/*.js',
-        './app/javascript/**/*.vue',
-        './app/javascript/**/*.jsx',
-      ],
-      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  )
-}
+// if (process.env.RAILS_ENV === 'production') {
+//   environment.plugins.push(
+//     require('@fullhuman/postcss-purgecss')({
+//       content: [
+//         './app/**/.html.erb',
+//         './app/helpers/**/*.rb',
+//         './app/javascript/**/*.js',
+//         './app/javascript/**/*.vue',
+//         './app/javascript/**/*.jsx',
+//       ],
+//       defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || []
+//     })
+//   )
+// }
 
 module.exports = environment;
